9210f4b2a734385ec240becffaf85090d55f590e
lamhaison <lamhaison@gmail.com>
2016-05-11 13:26:58 +0700

    remove fail unit test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,8 +11,6 @@ class BasicTestSuite(unittest.TestCase):
     def test_absolute_truth_and_meaning(self):
         assert True
 
-    def test_sum_method(self):
-        self.assertEqual(10, 5 +6)
 
 
 if __name__ == '__main__':
